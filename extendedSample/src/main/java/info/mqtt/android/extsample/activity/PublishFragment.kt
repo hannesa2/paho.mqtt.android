d6e37005fb0c11d0f0a1265389253e04f6b914a6
Hannes Achleitner <hannes.software@gmx.at>
2021-07-05 07:48:37 +0200

    Some Material EditText

--- extendedSample/src/main/java/info/mqtt/android/extsample/activity/PublishFragment.kt
+++ extendedSample/src/main/java/info/mqtt/android/extsample/activity/PublishFragment.kt
@@ -5,15 +5,9 @@ import info.mqtt.android.extsample.internal.Connections
 import timber.log.Timber
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.Spinner
-import android.text.TextWatcher
-import android.text.Editable
 import android.view.View
+import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import info.mqtt.android.extsample.R
@@ -23,8 +17,6 @@ class PublishFragment : Fragment() {
     private var connection: Connection? = null
     private var selectedQos = 0
     private var retainValue = false
-    private var topic = "/test"
-    private var message = "Hello world"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,21 +32,8 @@ class PublishFragment : Fragment() {
         val messageText = rootView.findViewById<EditText>(R.id.message)
         val qos = rootView.findViewById<Spinner>(R.id.qos_spinner)
         val retain = rootView.findViewById<SwitchCompat>(R.id.retain_switch)
-        topicText.setText(topic)
-        topicText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
-            override fun afterTextChanged(s: Editable) {
-                topic = s.toString()
-            }
-        })
-        messageText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
-            override fun afterTextChanged(s: Editable) {
-                message = s.toString()
-            }
-        })
+        topicText.setText(DEFAULT_TOTPIC)
+
         qos.onItemSelectedListener = object : OnItemSelectedListener {
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -70,9 +49,17 @@ class PublishFragment : Fragment() {
         qos.adapter = adapter
         val publishButton = rootView.findViewById<Button>(R.id.publish_button)
         publishButton.setOnClickListener {
-            Timber.d("Publishing: [topic: $topic, message: $message, QoS: $selectedQos, Retain: $retainValue]")
-            connection?.let { it1 -> (requireActivity() as MainActivity).publish(it1, topic, message, selectedQos, retainValue) }
+            Timber.d("Publishing: [topic: ${topicText.text}, message: ${messageText.text}, QoS: $selectedQos, Retain: $retainValue]")
+            connection?.let { it1 ->
+                (requireActivity() as MainActivity).publish(it1, topicText.text.toString(), messageText.text.toString(), selectedQos, retainValue)
+            } ?: run {
+                Toast.makeText(requireContext(), "Offline !", Toast.LENGTH_SHORT).show()
+            }
         }
         return rootView
+    }
+
+    companion object {
+        private const val DEFAULT_TOTPIC = "/test"
     }
 }
