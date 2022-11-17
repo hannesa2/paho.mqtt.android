284a0943d0efb8869f3eb4cd9b1db621edffc9c5
Hannes Achleitner <hannes.achleitner@googlemail.com>
2022-11-17 08:43:57 +0100

    Rework screenshot tests (#390)
    * Rework screenshot tests
    
    * More libs
    
    * BasicSample
    
    * extendedSample
    
    * Remove truth
    
    * remove androidx.test.ext:junit
    
    * Remove androidx.test:runner
    
    * remove androidx.test:rules
    
    * Remove androidx.test.espresso:espresso-core
    
    * Cleanup serviceLibrary

--- extendedSample/src/androidTest/java/info/mqtt/android/extsample/activity/ExtendedMainTest.kt
+++ extendedSample/src/androidTest/java/info/mqtt/android/extsample/activity/ExtendedMainTest.kt
@@ -1,41 +1,42 @@
 package info.mqtt.android.extsample.activity
 
-import android.Manifest
 import android.view.Gravity
-import androidx.test.espresso.Espresso.onView
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.GrantPermissionRule
-import com.moka.utils.Screenshot
-import com.moka.utils.ScreenshotActivityRule
 import info.mqtt.android.extsample.MainActivity
 import info.mqtt.android.extsample.R
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
 class ExtendedMainTest {
 
+    // a handy JUnit rule that stores the method name, so it can be used to generate unique screenshot files per test method
     @get:Rule
-    var mActivityTestRule = ScreenshotActivityRule(MainActivity::class.java)
+    var nameRule = TestName()
 
     @get:Rule
-    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.READ_EXTERNAL_STORAGE
-    )
+    val activityScenarioRule = activityScenarioRule<MainActivity>()
 
     @Test
     fun mainSmokeTest() {
-        onView(withId(R.id.drawer_layout))
+        Espresso.onView(withId(R.id.drawer_layout))
             .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
             .perform(DrawerActions.open())
-        Screenshot.takeScreenshot("End")
+        Espresso.onView(ViewMatchers.isRoot())
+            .captureToBitmap()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-End")
     }
 
 }
