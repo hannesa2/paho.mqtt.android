37cf17c6a197f41311326637adfe90f20821a8d6
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2023-11-15 11:34:14 +0700

    ktlint check

--- serviceLibrary/src/main/java/info/mqtt/android/service/extension/BundleExt.kt
+++ serviceLibrary/src/main/java/info/mqtt/android/service/extension/BundleExt.kt
@@ -6,12 +6,14 @@ import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.os.Parcelable
 
+@Suppress("DEPRECATION")
 inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
     SDK_INT >= 33 -> getParcelable(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+    else ->  getParcelable(key) as? T
 }
 
+@Suppress("DEPRECATION")
 inline fun <reified T : Serializable> Bundle.serializable(key: String): T? = when {
     SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializable(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getSerializable(key) as? T
+    else ->  getSerializable(key) as? T
 }
