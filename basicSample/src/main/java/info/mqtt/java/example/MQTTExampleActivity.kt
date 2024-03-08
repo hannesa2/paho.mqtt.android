12b9631c63bbd87114b1e9aaba56090c09748e83
Hannes Achleitner <hannes.software@gmx.at>
2024-03-08 08:19:28 +0100

    registerForActivityResult

--- basicSample/src/main/java/info/mqtt/java/example/MQTTExampleActivity.kt
+++ basicSample/src/main/java/info/mqtt/java/example/MQTTExampleActivity.kt
@@ -1,10 +1,21 @@
 package info.mqtt.java.example
 
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import info.mqtt.android.service.MqttAndroidClient
 import info.mqtt.android.service.QoS
@@ -22,6 +33,25 @@ class MQTTExampleActivity : AppCompatActivity() {
     private lateinit var mqttAndroidClient: MqttAndroidClient
     private lateinit var adapter: HistoryAdapter
     private lateinit var binding: ActivityScrollingBinding
+    private var hasNotificationPermissionGranted = false
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            hasNotificationPermissionGranted = isGranted
+            if (!isGranted) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Build.VERSION.SDK_INT >= 33) {
+                        if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                            showNotificationPermissionRationale()
+                        } else {
+                            showSettingDialog()
+                        }
+                    }
+                }
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "notification permission granted", Snackbar.LENGTH_LONG).setAction("Action", null)
+                    .show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +61,45 @@ class MQTTExampleActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        if ((Build.VERSION.SDK_INT >= 33) && (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                    PackageManager.PERMISSION_GRANTED)
+        ) {
+            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            hasNotificationPermissionGranted = true
+        }
+
         binding.fab.setOnClickListener { publishMessage() }
         val mLayoutManager: LayoutManager = LinearLayoutManager(this)
         binding.historyRecyclerView.layoutManager = mLayoutManager
         adapter = HistoryAdapter()
         binding.historyRecyclerView.adapter = adapter
         clientId += System.currentTimeMillis()
-        mqttAndroidClient = MqttAndroidClient(applicationContext, serverUri, clientId)
+
+        //mqttAndroidClient = MqttAndroidClient(applicationContext, serverUri, clientId)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, "MQTT", NotificationManager.IMPORTANCE_LOW)
+            channel.description = "MQTT Example Notification Channel"
+            channel.enableLights(true)
+            channel.lightColor = Color.GREEN
+            channel.enableVibration(false)
+            val notificationManager: NotificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+
+            val notificationBuilder: Notification.Builder =
+                Notification.Builder(this, CHANNEL_ID)
+                    .setContentTitle("MQTT Notification")
+                    .setContentText("Content title")
+//                    .setSmallIcon(R.drawable.ic_topic)
+            val foregroundNotification = notificationBuilder.build()
+            mqttAndroidClient = MqttAndroidClient(applicationContext, serverUri, clientId).apply {
+                setForegroundService(foregroundNotification)
+            }
+        } else {
+            mqttAndroidClient = MqttAndroidClient(applicationContext, serverUri, clientId)
+        }
+
         mqttAndroidClient.setCallback(object : MqttCallbackExtended {
             override fun connectComplete(reconnect: Boolean, serverURI: String) {
                 if (reconnect) {
@@ -128,11 +190,39 @@ class MQTTExampleActivity : AppCompatActivity() {
         }
     }
 
+    private fun showSettingDialog() {
+        MaterialAlertDialogBuilder(this, com.google.android.material.R.style.MaterialAlertDialog_Material3)
+            .setTitle("Notification Permission")
+            .setMessage("Notification permission is required, Please allow notification permission from setting")
+            .setPositiveButton("Ok") { _, _ ->
+                val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.parse("package:$packageName")
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun showNotificationPermissionRationale() {
+
+        MaterialAlertDialogBuilder(this, com.google.android.material.R.style.MaterialAlertDialog_Material3)
+            .setTitle("Alert")
+            .setMessage("Notification permission is required, to show notification")
+            .setPositiveButton("Ok") { _, _ ->
+                if (Build.VERSION.SDK_INT >= 33) {
+                    notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
     companion object {
         private const val serverUri = "tcp://broker.hivemq.com:1883"
         private const val subscriptionTopic = "exampleAndroidTopic"
         private const val publishTopic = "exampleAndroidPublishTopic"
         private const val publishMessage = "Hello World"
         private var clientId = "BasicSample"
+        private const val CHANNEL_ID = "BasicSampleNotificationChannelID"
     }
 }
