638c1327e12ee356ed3eb44db74c29e92a2ed509
Hannes Achleitner <hannes.software@gmx.at>
2024-05-21 07:29:36 +0200

    Long running test in sleep mode
    Remove Timber tree in Espresso

---
+++ extendedSample/src/androidTest/java/info/mqtt/android/extsample/activity/KeyEvent.kt
@@ -0,0 +1,7 @@
+package info.mqtt.android.extsample.activity
+
+// https://github.com/hannesa2/paho.mqtt.android/issues/604#issue-2236895469
+enum class KeyEvent(val eventKey: Int) {
+    SLEEP(223),
+    AWAKE(224)
+}
