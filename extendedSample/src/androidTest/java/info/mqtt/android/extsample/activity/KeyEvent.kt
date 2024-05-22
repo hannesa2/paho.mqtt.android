530fc56bd481aca16cc504f8f4b3717cc5bb4b25
Hannes Achleitner <hannes.software@gmx.at>
2024-05-22 06:42:20 +0200

    Use UiDevice to sleep/wakeup

--- extendedSample/src/androidTest/java/info/mqtt/android/extsample/activity/KeyEvent.kt
+++
@@ -1,7 +0,0 @@
-package info.mqtt.android.extsample.activity
-
-// https://github.com/hannesa2/paho.mqtt.android/issues/604#issue-2236895469
-enum class KeyEvent(val eventKey: Int) {
-    SLEEP(223),
-    AWAKE(224)
-}
