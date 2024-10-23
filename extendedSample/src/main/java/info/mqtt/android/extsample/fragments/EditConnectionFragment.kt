1bd76acabadb1db74b2ff30f7ab008e3b0513da5
Hannes Achleitner <hannes.software@gmx.at>
2024-10-23 09:35:42 +0200

    Apply lint suggestions

--- extendedSample/src/main/java/info/mqtt/android/extsample/fragments/EditConnectionFragment.kt
+++ extendedSample/src/main/java/info/mqtt/android/extsample/fragments/EditConnectionFragment.kt
@@ -38,7 +38,7 @@ class EditConnectionFragment : Fragment() {
         _binding = FragmentEditConnectionBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_dropdown_item, QoS.values())
+        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_dropdown_item, QoS.entries.toTypedArray())
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         if (this.arguments != null && requireArguments().getString(ActivityConstants.CONNECTION_KEY) != null) {
             val connections: HashMap<String, Connection> = getInstance(requireActivity()).connections
@@ -159,7 +159,7 @@ class EditConnectionFragment : Fragment() {
         })
         binding.lwtQosSpinner.onItemSelectedListener = object : OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-                formModel.lwtQos = QoS.values()[position]
+                formModel.lwtQos = QoS.entries.toTypedArray()[position]
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
