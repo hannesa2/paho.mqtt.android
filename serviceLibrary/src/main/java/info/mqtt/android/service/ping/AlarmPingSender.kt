9b843715aa7f0dcea3da5342e9fa2b2319510db7
Hannes Achleitner <hannes.software@gmx.at>
2025-04-25 07:27:04 +0200

    Run CI on forks properly

--- serviceLibrary/src/main/java/info/mqtt/android/service/ping/AlarmPingSender.kt
+++ serviceLibrary/src/main/java/info/mqtt/android/service/ping/AlarmPingSender.kt
@@ -22,11 +22,12 @@ import java.util.concurrent.TimeUnit
  *
  * @see MqttPingSender
  */
-internal class AlarmPingSender(val service: MqttService,
-                               val id: String,
-                               private val pingLogging: Boolean = false,
-                               private val keepPingRecords: Int = 1000) : MqttPingSender {
-
+internal class AlarmPingSender(
+    val service: MqttService,
+    val id: String,
+    private val pingLogging: Boolean = false,
+    private val keepPingRecords: Int = 1000
+) : MqttPingSender {
 
     private val workManager = WorkManager.getInstance(service)
 
