69b8313ddaf7ef35745c269b5e15e3d038698a4b
Hannes Achleitner <hannes.software@gmx.at>
2026-02-15 21:23:55 +0100

    Rename

--- extendedSample/src/main/java/info/mqtt/android/extsample/ActivityConstants.kt
+++ extendedSample/src/main/java/info/mqtt/android/extsample/ActivityConstants.kt
@@ -3,7 +3,7 @@ package info.mqtt.android.extsample
 internal object ActivityConstants {
     const val CONNECTION_KEY = "CONNECTION_KEY"
     const val CONNECTED = "CONNECTEd"
-    const val historyProperty = "history"
+    const val HISTORY_PROPERTY = "history"
     const val ConnectionStatusProperty = "connectionStatus"
     const val empty = ""
 }
